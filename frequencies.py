3bce48165adcb815946cad2362c47fe53337c261
Stephanie Lawal <stephanie.1.lawal@kcl.ac.uk>
2022-09-24 14:20:10 +0100

    Complete excercise and test locally

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,17 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in range (0,len(items)):
+        if isinstance(items[i], int) == True:
+            items[i] = f'{items[i]}'
+            if items[i] not in frequencies.keys():
+                frequencies[items[i]] = 1
+            else:
+                frequencies[items[i]] += 1
+        else:
+            if items[i] not in frequencies.keys():
+                frequencies[items[i]] = 1
+            else:
+                frequencies[items[i]] += 1
+
     return frequencies
